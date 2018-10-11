b88d28caf7b4c5620a427bd8093a9968cd6025d9
Daniel Cortés <dcortes@openknowledgenetwork.com>
2018-10-11 15:00:25 +0200

    Ember failing test case

---
+++ packages/@css-blocks/ember-cli/tests/dummy/app/components/inherit-state-component.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/inherit-state-component';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+  error: false,
+});
